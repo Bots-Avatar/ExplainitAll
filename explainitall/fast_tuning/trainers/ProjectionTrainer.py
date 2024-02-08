12f2c5c5f879f9999f79bc2549ba2e0853e5f9bf
Zahar A. Ponimash <ponimashz@mail.ru>
2024-02-08 19:05:49 +0300

    Create ProjectionTrainer.py

---
+++ explainitall/fast_tuning/trainers/ProjectionTrainer.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+from transformers import Trainer, TrainingArguments, GPT2Tokenizer
+from transformers import TextDataset, DataCollatorForLanguageModeling
+
+
+class GPTProjectionTrainer:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def set_variety(self, bias_mask, variety=0, min_prob=3e-3):
+        bias = np.zeros((self.model.lm_head.out_features,))
+        coef_mask = np.log2(variety + min_prob) / np.log2(np.e)
+        bias += coef_mask
+
+        for token in bias_mask:
+            bias[token] = 0
+
+        b_tensor = torch.tensor(bias, dtype=torch.float32)
+        out_gpt_layer = torch.nn.Linear(in_features=self.model.lm_head.in_features,
+                                        out_features=self.model.lm_head.out_features, bias=True)
+        out_gpt_layer.weight = self.model.lm_head.weight
+        out_gpt_layer.bias.data.copy_(b_tensor)
+        self.model.lm_head = out_gpt_layer
+
+    def load_dataset(self, file_path, block_size=256):
+        dataset = TextDataset(
+            tokenizer=self.tokenizer,
+            file_path=file_path,
+            block_size=block_size,
+        )
+        return dataset
+
+    def create_data_collator(self, mlm=False):
+        data_collator = DataCollatorForLanguageModeling(
+            tokenizer=self.tokenizer,
+            mlm=mlm,
+        )
+        return data_collator
+
+    def train(self, train_file_path, bias_mask, variety=0.0, output_dir="new_gpt", per_device_train_batch_size=2,
+              num_train_epochs=3, save_steps=1000, device=None):
+        self.set_variety(bias_mask, variety=variety)
+        self.model.to(device)
+
+        for name, param in self.model.named_parameters():
+            if "c_proj.weight" in name and "mlp" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+        train_dataset = self.load_dataset(train_file_path)
+        data_collator = self.create_data_collator()
+
+        training_args = TrainingArguments(
+            output_dir=output_dir,
+            overwrite_output_dir=True,
+            per_device_train_batch_size=per_device_train_batch_size,
+            num_train_epochs=num_train_epochs,
+            save_steps=save_steps,
+        )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            data_collator=data_collator,
+            train_dataset=train_dataset,
+        )
+
+        trainer.train()
